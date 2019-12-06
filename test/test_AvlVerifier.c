16aa5ca6366e84d1fabda9a0aba41532d760159a
owencho <owencho200@gmail.com>
2019-12-06 11:07:43 +0800

    modified avlverifier for delete, but now still not working

--- test/test_AvlVerifier.c
+++ test/test_AvlVerifier.c
@@ -37,13 +37,41 @@ void test_random_num(void){
     while(i--)
         printf("random num : %d \n",getRandomNum());
 }
-
+int getRandomDeleteNode(Node* childNode, int height){
+    int randNum;
+    while(height--){
+        randNum = getRandomNum();
+        if(randNum > childNode->value ){
+            if(childNode->right != NULL)
+                childNode=childNode->right;
+            else if (childNode-> left != NULL)
+                childNode=childNode->left;
+            else{
+                break;
+            }
+        }
+        else if(randNum < childNode->value ){
+            if(childNode->left != NULL)
+                childNode=childNode->left;
+            else if (childNode-> right != NULL)
+                childNode=childNode->right;
+            else{
+                break;
+            }
+        }
+        else {
+            break;
+        }
+    }
+    return childNode->value;
+}
 //srand(2)
 //5 √ 10 √ 100 √ 1000 √ 10000 √
 //time(0)
 //5 √ 10 √ 100 √ 1000 √ 10000 √
 
 //Automated Test for checking AVL add function
+/*
 void test_verify_AVL_tree(void){
     Node * root;
     int max = 10000 , i;
@@ -61,12 +89,13 @@ void test_verify_AVL_tree(void){
     }
     freeAllNodesInTree(root);
 }
-
+*/
 //Automated Test for checking AVL add and delete function
-void test_verify_AVL_tree(void){
-    Node * root;
+void test_verify_add_delete_AVL_tree(void){
+    Node * root ;
+    Node * childNode;
     int max = 10000 , i;
-    int height = log2(max)+5;
+    int height,deleteValue;
     srand(time(0));
     i = max;
     root =  createNode(getRandomNum(),  NULL ,NULL,0);
@@ -79,6 +108,17 @@ void test_verify_AVL_tree(void){
         }
         TEST_VERIFY_AVL_TREE(root, max-i+1);
     }
-
-    freeAllNodesInTree(root);
+    for(i=0;i<max;i++){
+        height= (getRandomNum()/log2(max)+5);
+        childNode = root;
+        deleteValue=getRandomDeleteNode(childNode,height);
+         Try{
+              root = avlDelete(root,deleteValue);
+          }Catch(ex){
+              dumpException(ex);
+              i--;
+          }
+          TEST_VERIFY_AVL_TREE(root, max-i);
+      }
+      freeAllNodesInTree(root);
 }
